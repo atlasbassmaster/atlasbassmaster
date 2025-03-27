7d7a3e31b934cb2d30916bc504793efc796df155
atlasbassmaster <echchadili.alaa@gmail.com>
2025-03-27 01:51:41 +0000

    Add docker-compose

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,18 +1,15 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-});
-
-export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Connecté à PostgreSQL");
-  } catch (error) {
-    console.error("❌ Erreur de connexion :", error);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false, // Désactive les logs SQL
   }
-};
+);
+
+export default sequelize;
